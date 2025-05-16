087952adf3255b5bd11790878563226a42d6b93e
tobiasonandia <tobiasonandia0@gmail.com>
2025-05-16 15:50:51 -0300

    add redis

--- utils/addTokenToBlockList.tsx
+++ utils/addTokenToBlockList.tsx
@@ -1,24 +1,24 @@
-import { redisClient } from "@/lib/redis";
+import { redis } from "@/lib/redis";
 
 export async function addTokenToBlockList(
   jti: string,
   exp: number
 ): Promise<void> {
-  // Asegúrate de que el cliente esté listo antes de usarlo
-  if (!redisClient.isReady) {
-    console.error("Redis client not ready. Cannot add token to blocklist.");
-    // Dependiendo de tu tolerancia, podrías lanzar un error o simplemente registrarlo
-    throw new Error("Servicio de lista negra no disponible.");
+  if (
+    !process.env.UPSTASH_REDIS_REST_URL ||
+    !process.env.UPSTASH_REDIS_REST_TOKEN
+  ) {
+    console.error(
+      "Redis configuration not available. Cannot add token to blocklist."
+    );
+    throw new Error("Redis service not configured.");
   }
 
   const nowInSeconds = Math.floor(Date.now() / 1000);
   const ttl = exp - nowInSeconds; // Tiempo de vida restante en segundos
 
-  // Solo agregamos a la blocklist si el token aún no ha expirado
   if (ttl > 0) {
-    // Usa el JTI como clave, almacena un valor dummy (ej: 'blocked') y establece la expiración
-    // La clave expirará automáticamente en Redis, limpiando la lista negra.
-    await redisClient.set(`blocked_jwt:${jti}`, "blocked", { EX: ttl });
+    await redis.set(`blocked_jwt:${jti}`, "blocked", { ex: ttl });
     console.log(`Token JTI ${jti} añadido a la lista negra con TTL ${ttl}s.`);
   } else {
     console.log(
