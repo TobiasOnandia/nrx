4f8e043ed1d43c23640388cf69a7f1a671cfba85
tobiasonandia <tobiasonandia0@gmail.com>
2025-05-16 15:36:36 -0300

    create: logoutAction

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,18 +1,27 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import { LoginSchema, RegisterSchema, ResponseAuth } from "@/types/schema/schema.auth";
+import {
+  LoginSchema,
+  RegisterSchema,
+  ResponseAuth,
+} from "@/types/schema/schema.auth";
+import { addTokenToBlockList } from "@/utils/addTokenToBlockList";
 import bcrypt from "bcryptjs";
-import { SignJWT } from "jose";
-import { nanoid } from 'nanoid'
+import { JWTPayload, jwtVerify, SignJWT } from "jose";
+import { JWTExpired, JWTInvalid } from "jose/errors";
+import { nanoid } from "nanoid";
 import { cookies } from "next/headers";
 
+const JWT_SECRET_STRING = process.env.JWT_SECRET;
+const JWT_SECRET_KEY = new TextEncoder().encode(JWT_SECRET_STRING);
+const JWT_EXPIRATION_TIME = "1h";
+const AUTH_TOKEN_COOKIE_NAME = "auth_token";
 
-
-const JWT_SECRET_STRING = process.env.JWT_SECRET
-const JWT_SECRET_KEY = new TextEncoder().encode(JWT_SECRET_STRING)
-const JWT_EXPIRATION_TIME = "1h"
-
+interface AuthTokenPayload extends JWTPayload {
+  jti?: string;
+  exp: number;
+}
 
 export async function registerAction(request: {
   email: string;
@@ -70,15 +79,13 @@ export async function registerAction(request: {
   }
 }
 
-
 export async function loginAction(request: {
-  email: string,
-  password: string
-}): Promise<ResponseAuth>{
+  email: string;
+  password: string;
+}): Promise<ResponseAuth> {
+  const validationResult = LoginSchema.safeParse(request);
 
- const validationResult = LoginSchema.safeParse(request);
-
- if (!validationResult.success) {
+  if (!validationResult.success) {
     const errorMessages = validationResult.error.errors
       .map((err) => err.message)
       .join(", ");
@@ -93,55 +100,106 @@ export async function loginAction(request: {
 
   try {
     const user = await prisma.user.findUnique({
-      where: { email }
-    })
+      where: { email },
+    });
 
-    if(!user) {
+    if (!user) {
       return {
         success: false,
-        message: "User not found"
-      }
+        message: "User not found",
+      };
     }
 
-    if(!await bcrypt.compare(password, user.password)){
-      return{
+    if (!(await bcrypt.compare(password, user.password))) {
+      return {
         success: false,
-        message: "Invalid credentials"
-      }
+        message: "Invalid credentials",
+      };
     }
 
-    const payload = { userId: user.id, email: user.email}
+    const payload = { userId: user.id, email: user.email };
     const token = await new SignJWT(payload)
-      .setProtectedHeader({ alg: 'HS256' })
+      .setProtectedHeader({ alg: "HS256" })
       .setIssuedAt()
       .setJti(nanoid())
       .setExpirationTime(JWT_EXPIRATION_TIME) // Por ejemplo, '1h'
       .sign(JWT_SECRET_KEY);
 
+    const ONE_HOUR_IN_ECONDS = 60 * 60;
 
-    const ONE_HOUR_IN_ECONDS = 60 * 60
-
-    const cookiesStore = await cookies()
+    const cookiesStore = await cookies();
     cookiesStore.set("auth_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: ONE_HOUR_IN_ECONDS,
-      path: "/"
-    })
+      path: "/",
+    });
 
     return {
       success: true,
       message: "login successfully",
       user: { id: user.id, email: user.email, name: user.name },
     };
-
-  }
-  catch(error) {
-  console.error(error);
+  } catch (error) {
+    console.error(error);
     return {
       success: false,
       message: "An error occurred while Loging",
     };
   }
+}
 
+export async function logoutAction(): Promise<ResponseAuth> {
+  const cookiesStore = await cookies();
+  const authToken = cookiesStore.get(AUTH_TOKEN_COOKIE_NAME);
+
+  if (!authToken) {
+    return {
+      success: true,
+      message: "logout successfully",
+    };
+  }
+
+  try {
+    const secret = new TextEncoder().encode(JWT_SECRET_STRING);
+    const { payload } = await jwtVerify(authToken.value, secret);
+
+    const jti = payload.jti;
+    const exp = payload.exp;
+
+    if (!jti || !exp) {
+      return {
+        success: true,
+        message: "logout successfully",
+      };
+    }
+
+    await addTokenToBlockList(jti, exp);
+    return {
+      success: true,
+      message: "logout successfully",
+    };
+  } catch (error) {
+    if (error instanceof JWTExpired) {
+      console.error("Token expired", error.message);
+      return {
+        success: true,
+        message: "Token expired",
+      };
+    }
+
+    if (error instanceof JWTInvalid) {
+      console.error("Invalid token", error.message);
+      return {
+        success: true,
+        message: "Invalid token",
+      };
+    }
+
+    console.error("Error verifying token", error);
+    return {
+      success: false,
+      message: "An error occurred while logging out",
+    };
+  }
 }
