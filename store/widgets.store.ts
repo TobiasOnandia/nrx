3f9147eed677b579b5b9c05a525d5ac7f2ca1092
Tobias Onandia <94480687+TobiasOnandia@users.noreply.github.com>
2025-05-20 10:54:45 -0300

    add store function widgets

---
+++ store/widgets.store.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+
+interface DashboardWidget {
+  id: string;
+  typeId: string;
+  x: number;
+  y: number;
+  w: number; 
+  h: number; 
+  config?: any;
+}
+
+interface WidgetsStore {
+  widgets: DashboardWidget[];
+  addWidget: (widget: DashboardWidget) => void;
+  updateWidgetsLayout: (newLayout: DashboardWidget[]) => void;
+  removeWidget: (id: string) => void;
+}
+
+export const useWidgetsStore = create<WidgetsStore>((set) => ({
+  widgets: [], 
+  addWidget: (widget) =>
+    set((state) => ({
+      widgets: [...state.widgets, widget], 
+    })),
+
+  updateWidgetsLayout: (newLayout) =>
+    set(() => ({
+      widgets: newLayout,
+    })),
+
+  removeWidget: (id) =>
+    set((state) => ({
+      widgets: state.widgets.filter((w) => w.id !== id), 
+    })),
+}));
