319e2f3b23a65dfddb4e90949b08a380bf98070c
Tobias Onandia <tobiasonandia0@gmail.com>
2025-05-26 16:17:19 -0300

    useMutation in ButtonUse

--- components/dashboard-actions/ButtonUse.tsx
+++ components/dashboard-actions/ButtonUse.tsx
@@ -1,31 +1,34 @@
-import { Check } from "lucide-react";
-import { setDefaultDashboard } from "@/app/actions/dashboard";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+'use client';
+import { Check, Loader2 } from 'lucide-react';
+import { setDefaultDashboard } from '@/app/actions/dashboard';
+import { toast } from 'sonner';
+import { useMutation } from '@tanstack/react-query';
 
 export const ButtonUse = ({ id }: { id: string }) => {
-  const router = useRouter();
-  const handleSetDefaultDashboard = async (dashboardId: string) => {
-    const response = await setDefaultDashboard({ id: dashboardId });
-    console.log(dashboardId);
-    if (!response.success) {
-      console.error("Failed to set default dashboard: ", response.message);
-      toast.error(response.message);
-      return;
-    }
-
-    toast.success("Dashboard set as default successfully");
-    router.refresh();
-  };
+  const setDefaultDashboardMutation = useMutation({
+    mutationFn: setDefaultDashboard,
+    onSuccess: () => {
+      toast.success('Dashboard set as default successfully');
+    },
+    onError: (error) => {
+      console.error('Failed to set default dashboard: ', error);
+      toast.error(`${error}`);
+    },
+  });
 
   return (
     <button
       className="p-2 cursor-pointer bg-emerald-600 hover:bg-emerald-500 rounded-lg"
       title="Use"
-      onClick={() => handleSetDefaultDashboard(id)}
+      onClick={() => setDefaultDashboardMutation.mutate({ id })}
+      disabled={setDefaultDashboardMutation.isPending}
       aria-label={`Use dashboard ${id}`}
     >
-      <Check className="w-4 h-4" />
+      {setDefaultDashboardMutation.isPending ? (
+        <Loader2 className="w-4 h-4 animate-spin" />
+      ) : (
+        <Check className="w-4 h-4" />
+      )}
     </button>
   );
 };
