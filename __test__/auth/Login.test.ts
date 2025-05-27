ea33c3ba079991648f1b82dc85bc35152874fa83
Tobias Onandia <tobiasonandia0@gmail.com>
2025-05-27 14:24:29 -0300

    add tests login

--- __test__/auth/Login.test.ts
+++ __test__/auth/Login.test.ts
@@ -6,9 +6,15 @@ import prisma from '@/lib/prisma';
 import { SignJWT } from 'jose';
 import { nanoid } from 'nanoid';
 import { cookies } from 'next/headers';
+import bcrypt from 'bcryptjs';
+
+const TEST_USER_ID = 'test-user-id';
+const TEST_USER_EMAIL = 'test-user@example.com';
+const TEST_USER_NAME = 'Test User';
+const TEST_JTI = 'mock_jti_123';
+
 vi.stubEnv('AUTH_TOKEN_COOKIE_NAME', 'test_auth_token');
 vi.stubEnv('JWT_SECRET', 'test_super_secret_jwt_key_32_chars');
-
 vi.mock('next/headers', () => ({
   cookies: vi.fn(),
 }));
@@ -27,6 +33,17 @@ vi.mock('jose', async (importOriginal) => {
   };
 });
 
+vi.mock('bcrypt', () => {
+  const compareMock = vi.fn();
+  const hashMock = vi.fn();
+
+  return {
+    default: {
+      compare: compareMock,
+      hash: hashMock,
+    },
+  };
+});
 vi.mock('nanoid', () => ({
   nanoid: vi.fn(),
 }));
@@ -43,13 +60,16 @@ describe('loginAction', () => {
   beforeEach(() => {
     vi.resetAllMocks();
     vi.spyOn(validationModule, 'validateAndExtract' as any);
+    const setCookieMock = vi.fn();
+
+    vi.mocked(cookies).mockReturnValue(
+      Promise.resolve({
+        set: setCookieMock,
+      }) as any,
+    );
   });
 
   it('should return a validation error if input is invalid', async () => {
-    const validationErrors = [
-      { path: ['email'], message: 'Invalid email format' },
-    ];
-
     vi.mocked(validationModule.validateAndExtract).mockReturnValueOnce({
       success: false,
       message: 'Validation failed',
@@ -79,5 +99,93 @@ describe('loginAction', () => {
     expect(SignJWT).not.toHaveBeenCalled();
     expect(nanoid).not.toHaveBeenCalled();
     expect(cookies).not.toHaveBeenCalled();
+  });
+
+  it('should return a user not found error if user is not found', async () => {
+    const request = { email: 'test-user@example.com', password: 'password' };
+
+    vi.mocked(validationModule.validateAndExtract).mockReturnValueOnce({
+      success: true,
+      data: request,
+    });
+
+    vi.mocked(prisma.user.findUnique).mockResolvedValueOnce(null);
+
+    const result = await loginAction(request);
+
+    expect(result).toEqual({
+      success: false,
+      message: 'User not found',
+    });
+
+    expect(validationModule.validateAndExtract).toHaveBeenCalledTimes(1);
+    expect(validationModule.validateAndExtract).toHaveBeenCalledWith(
+      LoginSchema,
+      request,
+    );
+    expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+    expect(prisma.user.findUnique).toHaveBeenCalledWith({
+      where: { email: request.email },
+    });
+    expect(SignJWT).not.toHaveBeenCalled();
+    expect(nanoid).not.toHaveBeenCalled();
+    expect(cookies).not.toHaveBeenCalled();
+  });
+
+  it('should return a user if found and password is correct', async () => {
+    const request = { email: TEST_USER_EMAIL, password: 'password' };
+
+    vi.mocked(validationModule.validateAndExtract).mockReturnValueOnce({
+      success: true,
+      data: request,
+    });
+
+    vi.mocked(prisma.user.findUnique).mockResolvedValueOnce({
+      id: TEST_USER_ID,
+      email: TEST_USER_EMAIL,
+      password: 'hashed_password_from_db',
+      name: TEST_USER_NAME,
+      createAt: new Date(),
+      updateAt: new Date(),
+    });
+
+    vi.spyOn(bcrypt, 'compare').mockResolvedValueOnce(true as any);
+
+    vi.mocked(nanoid).mockReturnValueOnce(TEST_JTI);
+    const result = await loginAction(request);
+
+    expect(result).toEqual({
+      success: true,
+      message: 'login successfully',
+      user: {
+        id: TEST_USER_ID,
+        email: TEST_USER_EMAIL,
+        name: TEST_USER_NAME,
+      },
+    });
+
+    expect(validationModule.validateAndExtract).toHaveBeenCalledTimes(1);
+    expect(validationModule.validateAndExtract).toHaveBeenCalledWith(
+      LoginSchema,
+      request,
+    );
+    expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+    expect(prisma.user.findUnique).toHaveBeenCalledWith({
+      where: { email: request.email },
+    });
+    expect(bcrypt.compare).toHaveBeenCalledTimes(1);
+    expect(bcrypt.compare).toHaveBeenCalledWith(
+      request.password,
+      'hashed_password_from_db',
+    );
+    expect(SignJWT).toHaveBeenCalledTimes(1);
+    expect(SignJWT).toHaveBeenCalledWith({
+      userId: TEST_USER_ID,
+      email: TEST_USER_EMAIL,
+    });
+    expect(nanoid).toHaveBeenCalledTimes(1);
+    expect(nanoid).toHaveBeenCalledWith();
+
+    expect(cookies).toHaveBeenCalledTimes(1);
   });
 });
